e5cd7a7486594b635008548d835b2b193051b045
prem-jethwa <premjethwa788@gmail.com>
2020-10-10 15:52:45 +0530

    Updates

---
+++ precache-manifest.1ee9e1481322a7522d26e72c01f153e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5048b5ccc70d9c76d2b7d7c8d3e08a07",
+    "url": "/react-main/index.html"
+  },
+  {
+    "revision": "137d6e51733de29361b5",
+    "url": "/react-main/static/css/main.7d86e859.chunk.css"
+  },
+  {
+    "revision": "b06cd0169be9a6c04a21",
+    "url": "/react-main/static/js/2.526395d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-main/static/js/2.526395d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "137d6e51733de29361b5",
+    "url": "/react-main/static/js/main.7b88faf3.chunk.js"
+  },
+  {
+    "revision": "5dfadad6773069bd2739",
+    "url": "/react-main/static/js/runtime-main.4ded883b.js"
+  }
+]);
